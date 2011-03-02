898cb2db001a9d66621239326a1c2235bcc37770
Mick Staugaard <mick@staugaard.com>
2011-03-02 11:38:29 -0800

    implemented a heat map

--- public/js/canvas_overlay.js
+++ public/js/canvas_overlay.js
@@ -69,3 +69,86 @@ ImageOverlay.prototype.beforeDrawDot = function(point) {
 ImageOverlay.prototype.drawDot = function(point) {
   this.canvasContext.drawImage(this.dot, point.x - 8, point.y - 8);
 };
+
+
+/*
+  Heat Map Overlay
+*/
+
+function HeatMapOverlay(map) {
+  this.setMap(map);
+};
+HeatMapOverlay.prototype = new CanvasOverlay();
+
+HeatMapOverlay.prototype.beforeDrawDot = function(point) {
+  var image = this.canvasContext.getImageData(0, 0, 1024, 600);
+  var imageData = image.data;
+  var length = imageData.length;
+  var value;
+  for(var i = 3; i < length; i += 4) {
+    imageData[i] = imageData[i] * 0.98;
+  };
+  image.data = imageData;
+  this.canvasContext.putImageData(image, 0, 0);
+};
+
+HeatMapOverlay.prototype.drawDot = function(point) {
+  var radius1 = 10;
+  var radius2 = 20;
+  var rgr = this.canvasContext.createRadialGradient(point.x, point.y, radius1, point.x, point.y, radius2);
+  // the center of the radial gradient has .1 alpha value
+  rgr.addColorStop(0, 'rgba(0,0,0,0.4)');
+  // and it fades out to 0
+  rgr.addColorStop(0.9, 'rgba(0,0,0,0)');
+  // drawing the gradient
+  this.canvasContext.fillStyle = rgr;
+  this.canvasContext.fillRect(point.x - radius2, point.y - radius2, 2 * radius2, 2 * radius2);
+  this.colorize(point.x - radius2, point.y - radius2, 2 * radius2);
+};
+
+HeatMapOverlay.prototype.colorize = function(x,y,x2) {
+  var image = this.canvasContext.getImageData(0, 0, 1024, 600);
+  // some performance tweaks
+  var imageData = image.data;
+  var length = imageData.length;
+  // loop thru the area
+  var r,g,b,tmp,alpha = 0;
+  for(var i = 3; i < length; i += 4) {
+    r = 0
+    g = 0;
+    b = 0;
+    tmp = 0;
+    // [0] -> r, [1] -> g, [2] -> b, [3] -> alpha
+    alpha = imageData[i];
+
+    // coloring depending on the current alpha value
+    if(alpha <= 255 && alpha >= 235) {
+      tmp = 255 - alpha;
+      r   = 255 - tmp;
+      g   = tmp * 12;
+    } else if(alpha <= 234 && alpha >= 200) {
+      tmp = 234 - alpha;
+      r   = 255 - (tmp * 8);
+      g   = 255;
+    } else if(alpha <= 199 && alpha >= 150) {
+      tmp = 199 - alpha;
+      g   = 255;
+      b   = tmp * 5;
+    } else if(alpha <= 149 && alpha >= 100) {
+      tmp = 149 - alpha;
+      g   = 255 - (tmp * 5);
+      b   = 255;
+    } else {
+      b  = 255;
+    }
+    // we ve started with i=3
+    // set the new r, g and b values
+    imageData[i-3] = r;
+    imageData[i-2] = g;
+    imageData[i-1] = b;
+  }
+  // the rgb data manipulation didn't affect the ImageData object(defined on the top)
+  // after the manipulation process we have to set the manipulated data to the ImageData object
+  image.data = imageData;
+  this.canvasContext.putImageData(image, 0, 0);
+};
